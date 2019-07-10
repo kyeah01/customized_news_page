c080f09b181e6c54165d039485285cdd0621f7c8
kimevanjunseok <jnunseok@gmail.com>
2019-07-10 12:14:45 +0900

    로그인 구현

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,29 @@
 <template>
   <v-app>
     <Navbar></Navbar>
+    <Login/>
+    <LoginCheck/>
+    <GoogleLogin/>
+    <FacebookLogin/>
     <router-view></router-view>
   </v-app>
 </template>
 
 <script>
 import Navbar from './components/Navbar'
+import Login from './components/Login'
+import LoginCheck from './components/LoginCheck'
+import GoogleLogin from './components/GoogleLogin'
+import FacebookLogin from './components/FacebookLogin'
 
 export default {
   name: 'App',
   components: {
-    Navbar
+    Navbar,
+    Login,
+    LoginCheck,
+    GoogleLogin,
+    FacebookLogin,
   },
   data () {
     return {
---
+++ src/components/FacebookLogin.vue
@@ -0,0 +1,40 @@
+<template>
+    <div>
+        <v-btn v-on:click="FacebookLogin">Facebook Login</v-btn>
+    </div>
+</template>
+
+
+<script>
+import firebase from 'firebase'
+import FirebaseService from '@/services/FirebaseService'
+
+export default {
+    data () {
+        return {
+        }
+    },
+    methods: {
+        FacebookLogin: function() {
+            var provider = new firebase.auth.FacebookAuthProvider();
+            firebase.auth().signInWithPopup(provider).then(function(result) {
+            // This gives you a Facebook Access Token. You can use it to access the Facebook API.
+            var token = result.credential.accessToken;
+            // The signed-in user info.
+            var user = result.user;
+            // ...
+            }).catch(function(error) {
+            // Handle Errors here.
+            var errorCode = error.code;
+            var errorMessage = error.message;
+            // The email of the user's account used.
+            var email = error.email;
+            // The firebase.auth.AuthCredential type that was used.
+            var credential = error.credential;
+            // ...
+            });
+
+        },
+    }
+}
+</script>
