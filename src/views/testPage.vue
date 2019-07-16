0fa61862b9298d9033a540d41f55739b2f8d5b61
이호진 <hojin1357@naver.com>
2019-07-16 11:15:08 +0900

    article 스크롤

--- src/views/testPage.vue
+++ src/views/testPage.vue
@@ -1,46 +1,40 @@
-<template lang="html">
-  <div id="graph-container">
-
-
+<template>
+  <div>
+    <v-layout column justify-center align-center>
+      <v-subheader>Offset Top</v-subheader>
+      {{ offsetTop }}
+    </v-layout>
+    <v-container
+      id="scroll-target"
+      style="max-height: 400px"
+      class="scroll-y"
+    >
+      <v-layout
+        v-scroll:#scroll-target="onScroll"
+        column
+        align-center
+        justify-center
+        style="height: 1000px"
+      >
+      </v-layout>
+    </v-container>
   </div>
 </template>
 
 <script>
-import { log } from 'util';
-import cube from '../timeCheck'
-import calcDiffOfDates from '../calcDiffOfDates'
+import Vue from 'vue'
+import vuescroll from 'vue-scroll'
+Vue.use(vuescroll)
 
-export default {
-  data(){
-    return {
-      sDate:null,
-      eDate:null,
-      diffDate:null
+  export default {
+    data: () => ({
+      offsetTop: 0
+    }),
+
+    methods: {
+      onScroll (e) {
+        this.offsetTop = e.target.scrollTop
+      }
     }
-  },
-  created(){
-    this.sDate = cube()
-    console.log(this.sDate);
-    
-    // console.log(`sDate : ${this.sDate.hour}:${this.sDate.min}:${this.sDate.second}`)    
-  },
-
-  destroyed(){
-    console.log('destroy');
-    this.eDate = cube()
-    // console.log(`eDate : ${this.eDate.hour}:${this.eDate.min}:${this.eDate.second}`)
-    // console.log(this.eDate);
-    
-
-    this.diffDate = calcDiffOfDates(this.sDate, this.eDate)
-    console.log(this.diffDate);
-    
-    // console.log(this.diffDate);
-    
   }
-
-}
 </script>
-
-<style lang="css" scoped>
-</style>
