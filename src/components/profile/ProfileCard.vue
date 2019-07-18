6762219c21c4956d1bb20141bb0087d121923880
이호진 <hojin1357@naver.com>
2019-07-18 16:00:49 +0900

    마이페이지 수정

---
+++ src/components/profile/ProfileCard.vue
@@ -0,0 +1,89 @@
+<template>
+    <v-card>
+        <v-container>
+          <v-divider color="primary"/>
+          <v-layout wrap now>
+            <v-flex xs4>
+              <span class="grey--text">{{title}}</span>
+              <v-avatar>
+                <v-icon medium>{{icon}}</v-icon>
+              </v-avatar>
+            </v-flex>
+          </v-layout>
+          <v-card-actions>
+             <v-btn 
+              text color="green" 
+              @click="test"
+              >
+              Detail More
+            </v-btn>
+          </v-card-actions>
+        </v-container>
+
+        <div>
+          <General v-if="this.dir===1"
+            :drawer = parentDrawer
+            @right_drawer = "update">
+          </General>
+          <!-- <Preview v-if="this.dir===2"
+            :drawer = parentDrawer
+            @right_drawer = "update">
+          </Preview>
+          <Intergrations v-else-if="this.dir===3"
+            :drawer = parentDrawer
+            @right_drawer = "update">
+          </Intergrations>
+          <Read v-else-if="this.dir===4"
+            :drawer = parentDrawer
+            @right_drawer = "update">
+          </Read>
+          <Appearance v-else-if="this.dir===5"
+            :drawer = parentDrawer
+            @right_drawer = "update">
+          </Appearance> -->
+          <UserSetting v-else
+            :drawer = parentDrawer
+            @right_drawer = "update">
+          </UserSetting>
+
+        </div>
+    </v-card>
+
+</template>
+
+<script>
+import General from '@/components/profile/General'
+// import Preview from '@/components/profile/Preview'
+// import Intergrations from '@/components/profile/Intergrations'
+// import Read from '@/components/profile/Read'
+// import Appearance from '@/components/profile/Appearance'
+import UserSetting from '@/components/profile/UserSetting'
+export default {
+  props : ['title','icon','event'],
+  components: {
+      General,
+      // UserSetting,
+      // Preview,
+      // Read,
+      // Appearance,
+      UserSetting
+  },
+  data(){
+    return{
+      parentDrawer:false,
+      dir : this.event
+    }
+  },
+  methods : {
+    test(){
+      this.update()
+    },
+    update(){
+      this.parentDrawer = !this.parentDrawer
+    }
+  }
+}
+</script>
+
+
+
