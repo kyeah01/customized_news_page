c86a1c5efa187140e1292ab0bd365a1bdce15d97
Haeje Park <hj123190@gmail.com>
2019-08-13 16:22:59 +0900

    navbar search 아이콘 애니메이션 수정

--- front/src/components/Navbar.vue
+++ front/src/components/Navbar.vue
@@ -10,9 +10,11 @@
             <div class="box" v-if="user">
                 <div class="container-1">
                     <br style="height: 20.8px;">
-                    <span class="icon" id="searchIcon"><i class="fa fa-search"></i></span>
                     
-                    <input v-model="searchWord" type="search" id="search" :placeholder="placeholder" @click="changePlaceholder" @keydown.enter="search" />
+                    <span class="icon" :class="{ focusOnNavSearch : isFocusOnNavSearch}" id="searchIcon"><i class="fa fa-search"></i></span>
+                    <input v-model="searchWord" type="search" id="search" :placeholder="placeholder" @click="changePlaceholder" @keydown.enter="search" >
+                        
+                    </input>
                 </div>
                 
             </div>
@@ -175,9 +177,11 @@ export default {
             readlaterArticles: null,
             recentlyReadDrawer: false,
             markasreadArticles: null,
+
             // searchSelected : "",
             searchSelected : "신문사",
             searchMenuSelect : ['키워드', '신문사'],
+            isFocusOnNavSearch : false,
         }
     },
     methods: {
@@ -321,10 +325,12 @@ export default {
                 var placeholder1 = $('#search');
                 placeholder1.focus(function () {
                     // placeholder1.val('Search in your feeds')
+                    that.isFocusOnNavSearch = true;
                     document.getElementById("search").placeholder = "Search in your feeds";
                 })
                 placeholder1.blur(function () {
                     // placeholder1.val('Search...')
+                    that.isFocusOnNavSearch = false;
                     document.getElementById("search").placeholder = "Search...";
                     that.searchWord = ''
                 })
@@ -511,32 +517,41 @@ export default {
     position: relative;
     right: 161px;
 } */
-.container-1:focus .icon,
+/* .container-1:focus .icon,
 .container-1:active .icon {
     transform: translateX(-105.5px)
+} */
+
+.container-1:focus > .icon{
+    transform: translateX(-105.5px);
+    color:red;
 }
 
-.container-1 .icon:focus,
+.focusOnNavSearch{
+    transform: translateX(-105.5px);
+}
+/* #search > .icon{
+        color:red;
+
+} */
+
+/* .container-1 .icon:focus,
 .container-1 .icon:active {
     transform: translateX(-105.5px)
-}
+} */
 
-.container-1 .icon:focus,
-.container-1 .icon:active:after {
-    transform: translateX(-105.5px)
-}
 
-.container-1 span#searchIcon:focus,
+/* .container-1 span#searchIcon:focus,
 .container-1 span#searchIcon:active {
     position: absolute;
     right: 270px;
-}
+} */
 
-.container-1 span#searchIcon:active:after {
+/* .container-1 span#searchIcon:active:after {
     position: absolute;
     right: 270px;
 
-}
+} */
 
 .container-1:hover input#search {
     /* width: 300px; */
