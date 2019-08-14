9c7a2644ba5d8c8abadb781e2bcc94b0b22bc5b4
HSx3 <hs.ssafy@gmail.com>
2019-08-14 10:29:44 +0900

    article 중복 수정

--- front/env.js
+++ front/env.js
@@ -5,8 +5,8 @@ var API = process.env.VUE_APP_APIKEY1
 export default {
     data() {
         return {
-            api: '2dc4b8b9d26f4a6b97e21a1f282bac9d',
-            api2: '8b64e14d415f40f2a7d2969321afc5f9',
+            api2: '2dc4b8b9d26f4a6b97e21a1f282bac9d',
+            api: '8b64e14d415f40f2a7d2969321afc5f9',
             api3: '9a26146bdfbb4679a1c5fcd25d168a8b',
             api4: '04622f774c8e47ad957691cf4e71eb9a'
         }
